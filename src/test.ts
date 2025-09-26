de0f03349b850f7a3d9847029ce37d664bae3663
Tom Guttermann <tom.guttermann@leanix.net>
2025-09-26 13:25:19 +0200

    Adjust test

--- src/test.ts
+++ src/test.ts
@@ -1,17 +1,35 @@
 import { slackClient } from "./slack/client.js";
-import { searchMessagesTool } from "./tools/searchMessages.js";
+import { getMessageThreadTool, GetMessageThreadParams } from "./tools/getMessageThread.js";
+import { searchMessagesTool, SearchMessagesParams } from "./tools/searchMessages.js";
 
 
 
 async function main() {
     // get slack client
     const slack = slackClient;
-    // const allowedChannels = slack.listAllowedChannels();
-    // console.log(allowedChannels);
 
     // call search messages tool
-    const result = await searchMessagesTool(slack, { query: "langfuse", pageLimit:1 });
-    console.log(JSON.stringify(result, null, 2));
+    const searchParams: SearchMessagesParams = { 
+        query: "langfuse",
+        messageCount: 1,
+        includeThreads: false,
+        threadCount: 10,
+        sortMessages: "mostRelevant"
+    };
+    const messageSearchResult = await searchMessagesTool(slack, searchParams);
+    console.log(JSON.stringify(messageSearchResult, null, 2));
+    console.log(`Got ${messageSearchResult.messages.length} messages`);
+    const message = messageSearchResult.messages[0];
+    const messageChannel = message.channelId;
+    const messageId = message.ts;
+    const messageDetailsParams: GetMessageThreadParams = {
+        channelId: messageChannel,
+        ts: messageId,
+        threadCount: 50
+    };
+    const messageDetailsResult = await getMessageThreadTool(slack, messageDetailsParams);
+    console.log(JSON.stringify(messageDetailsResult, null, 2));
+
 }
 
 main();
